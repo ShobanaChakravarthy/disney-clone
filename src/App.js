198c8787a8b80c76456db5f29b8375b0f92e55ca
shobana <shobana.chakravarthy@gmail.com>
2021-11-17 15:18:03 +0530

    ADD: Detail

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Detail from './components/Detail';
 import Header from './components/Header';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/detail' element={<Detail />} />
+          <Route path='/' exact element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
